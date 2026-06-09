99c5316528ff381c84a219eaf473d9dbd80920ac
chicken-eng <60812096+chicken-eng@users.noreply.github.com>
2026-06-09 10:21:24 +0100

    Update fsi_ai.py

--- fsi_ai.py
+++ fsi_ai.py
@@ -19,7 +19,7 @@ from database import get_db
 @st.cache_resource
 def get_llm():
     return ChatGroq(
-        model="llama-3.3-70b-versatile",
+        model="deepseek-r1-distill-llama-70b",
         groq_api_key=st.secrets["GROQ_API_KEY"],
         temperature=0
     )
